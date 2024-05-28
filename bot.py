3fee77b4a66a043da6978ee857c78034fdf5bd74
shrisharanyan <spellsharp>
2024-05-28 18:47:20 +0530

    Changed to slash_commands

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
 import discord
+from discord import app_commands
 from discord.ext import commands
 from discord.ui import View
 from ui_components import MovieButton, Movie, TVShow, SeriesButton
@@ -7,33 +8,39 @@ from dotenv import load_dotenv
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents, help_command=None)
-
 
 load_dotenv()
 omdb_token = os.getenv('OMDB_TOKEN')
+bot_token = os.getenv('DISCORD_TOKEN')
+application_id = os.getenv("APPLICATION_ID")
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
 
-@bot.command()
-async def help(ctx):
+
+
+
+@bot.tree.command(name="help", description='List of commands')
+async def help(interaction: discord.Interaction):
     print("help")
-    embed = discord.Embed(title='Help', description='List of commands', color=discord.Color.blue())
+    embed = discord.Embed(title='Help',
+                          description='List of commands',
+                          color=discord.Color.blue())
     embed.add_field(name='movie', value='`/movie Dune`', inline=False)
     embed.add_field(name='tvshow', value='`/tvshow Seinfeld`', inline=False)
-    await ctx.reply(embed=embed)
+    await interaction.response.send_message(embed=embed)
 
-@bot.command()
-async def movie(ctx, *, name=None):
+
+@bot.tree.command(name="movie", description='Search for a movie')
+async def movie(interaction: discord.Interaction, *, name: str = None):
     if not name:
-        embed = discord.Embed(title='Error', description='Please provide a TV show name', color=discord.Color.red())
-        await ctx.reply(embed=embed)
+        embed = discord.Embed(title='Error',
+                              description='Please provide a TV show name',
+                              color=discord.Color.red())
+        await interaction.response.send_message(embed=embed)
         return
     try:
-        response = requests.get(f"http://www.omdbapi.com/?apikey={omdb_token}&s={name}&type=movie")
+        response = requests.get(
+            f"http://www.omdbapi.com/?apikey={omdb_token}&s={name}&type=movie")
         data = response.json()['Search']
         print(f"/movie {name}")
         try:
@@ -42,38 +49,50 @@ async def movie(ctx, *, name=None):
                 movie = Movie(imdb_id=movie['imdbID'], name=movie['Title'])
                 movie_button = MovieButton(movie=movie)
                 view.add_item(movie_button)
-            await ctx.reply(view=view)
+            await interaction.response.send_message(view=view)
 
         except Exception as e:
             print(e)
     except Exception as e:
         print(e)
-        embed = discord.Embed(title=f'{name}', description='Movie not found', color=discord.Color.red())
-        await ctx.reply(embed=embed)
+        embed = discord.Embed(title=f'{name}',
+                              description='Movie not found',
+                              color=discord.Color.red())
+        await interaction.response.send_message(embed=embed)
         return
-        
-@bot.command()
-async def tvshow(ctx, *, name=None):
+
+
+@bot.tree.command(name='tvshow', description='Search for a TV show')
+async def tvshow(interaction: discord.Interaction, *, name: str = None):
     if not name:
-        embed = discord.Embed(title='Error', description='Please provide a TV show name\nExample: `$tvshow breaking bad`', color=discord.Color.red())
-        await ctx.reply(embed=embed)
+        embed = discord.Embed(
+            title='Error',
+            description=
+            'Please provide a TV show name\nExample: `/tvshow breaking bad`',
+            color=discord.Color.red())
+        await interaction.response.send_message(embed=embed)
         return
 
     try:
-        response = requests.get(f"http://www.omdbapi.com/?apikey={omdb_token}&s={name}&type=series")
+        response = requests.get(
+            f"http://www.omdbapi.com/?apikey={omdb_token}&s={name}&type=series"
+        )
         data = response.json()['Search']
         print(f"/tvshow {name}")
         try:
-            view =  View(timeout=None)
+            view = View(timeout=None)
             for series in data:
                 tvshow = TVShow(series=series)
-                series_button = SeriesButton(tvshow=tvshow, ctx=ctx, bot=bot)
+                series_button = SeriesButton(tvshow=tvshow, ctx=interaction, bot=bot)
                 view.add_item(series_button)
-            await ctx.reply(view=view)
+            await interaction.response.send_message(view=view)
         except Exception as e:
             print(e)
 
-    except:
-        embed = discord.Embed(title=f'{name}', description='TV Show not found', color=discord.Color.red())
-        await ctx.reply(embed=embed)
+    except Exception as e:
+        print(e)
+        embed = discord.Embed(title=f'{name}',
+                              description='TV Show not found',
+                              color=discord.Color.red())
+        await interaction.response.send_message(embed=embed)
         return
--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from dotenv import load_dotenv
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced commands: {synced} commands")
+    except Exception as e:
+        print(e)
+
 load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
 bot.run(discord_token)
